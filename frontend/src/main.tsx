fef7d0af0337603476e1a1cc268d583f16cab62f
Victor Voronov <vav0701@mail.ru>
2024-05-30 22:07:17 +0500

    style for cards, header and footer

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import {GlobalStyle} from "./styles/global.ts";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+      <GlobalStyle></GlobalStyle>
   </React.StrictMode>,
 )

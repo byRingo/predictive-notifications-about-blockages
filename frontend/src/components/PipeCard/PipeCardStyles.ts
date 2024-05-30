b35e887c95eb4327c1dead9f4dc22ff6a145a73b
Victor Voronov <vav0701@mail.ru>
2024-05-30 22:14:58 +0500

    project structure refactoring

---
+++ frontend/src/components/PipeCard/PipeCardStyles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    background-color: white;
+    height: 18rem;
+    padding: 3rem;
+    border-radius: 1rem;
+`
+
+export const Title = styled.span`
+text-align: center;
+    font-size: 1.2rem;
+    margin-top: -1.5rem;
+`
+
+export const Description = styled.span`
+    text-align: center;
+`
---
+++ frontend/src/pages/App/AppStyles.ts
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const Logo = styled.img`
+        width: 64px;
+        margin: 1rem;
+    `
+
+export const CompanyName = styled.span`
+      font-weight: 700;
+      font-size: 1.5rem;
+  `
+
+export const Main = styled.main`
+    flex-grow: 1;
+    background-color: #EAEAEA;
+  `
+
+export const Footer = styled.div`
+    display: flex;
+      background-color:#C9C9C9;
+    height: 70px;
+    justify-content: center;
+    align-items: center;
+    flex-shrink: 0;
+  `
+
+export const Header = styled.div`
+    display: flex;
+    height: 100px;
+    background-color: #C9C9C9;
+    gap: 2rem;
+    align-items: center;
+  `
+
+export const ResidentialComplexSection = styled.section`
+    display: flex;
+    flex-direction: column;
+    margin: 1rem;
+  `
+
+export const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+    min-height: 100vh;
+  `
+
+export const CardSection = styled.section`
+    display: flex;
+        gap: 2rem;
+    justify-content: center;    
+    `
